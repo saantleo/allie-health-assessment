7bd5836664d96c4550d745313c971cf89b6577ba
saantleo <devleonardoandrade@gmail.com>
2024-04-02 21:31:53 -0300

    feat: add user birthday

--- client/src/components/create/CreateForm.tsx
+++ client/src/components/create/CreateForm.tsx
@@ -1,13 +1,14 @@
 import { Alert, Box, Button, TextField } from "@mui/material";
 import useAxios from "axios-hooks";
 import { FieldValues, useForm } from "react-hook-form";
+import { User } from "../../models/User";
 
 type Props = {
   onSubmit: () => void;
 };
 
 const CreateForm = ({ onSubmit }: Props) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<User>();
   const [{ loading, error }, executePost] = useAxios(
     {
       url: `${process.env.REACT_APP_SERVER_BASE_URL}/users`,
@@ -50,6 +51,7 @@ const CreateForm = ({ onSubmit }: Props) => {
             {...register("lastName")}
           />
           <TextField label="Email" variant="outlined" {...register("email")} />
+          <TextField type="date" label="Birthday" variant="outlined" {...register("birthday")} />
           <Button variant="contained" type="submit" disabled={loading}>
             Create User
           </Button>
